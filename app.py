b5e2ca8986d18a133ae47093f6f6a9fa429448ce
Kenia <23308060610371@cetis61.edu.mx>
2025-10-30 11:01:18 -0500

    x

--- app.py
+++ app.py
@@ -5,6 +5,8 @@ app=Flask(__name__)
 
 @app.route("/inicio", methods=["POST"])
 def inicio():
+    if'alimentos'not in session:
+        session['alimentos']=[]
     if request.methods=='POST':
         nombre = request.form("nombre")
 grasas=float(request.form['grasas'])
@@ -16,7 +18,12 @@ p=proteinas * 4
 c= carbohidratos * 4
 
 if g>p and g>c:
+    tipo="Fuente de grasa"
 elif p>g and p>c:
+    tipo="Fuente de proteina"
+    else
+    tipo="Fuente de carbohidratos"
+    session['alimentos'].append({'nombre':nombre,'tipo'=tipo})
 return render_template('inicio.html')
 
 
